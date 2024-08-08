60bbb05177b31955dea0bdebab512b5ae9ea30c6
noahmik <s46422574@gmail.com>
2024-08-08 18:34:58 +0900

    FEAT :: 이슈 템플릿 작성

---
+++ src/main/java/com/boundary/boundarybackend/BoundarybackendApplication.java
@@ -0,0 +1,13 @@
+package com.boundary.boundarybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoundarybackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoundarybackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boundary/boundarybackend/BoundarybackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boundary.boundarybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoundarybackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
