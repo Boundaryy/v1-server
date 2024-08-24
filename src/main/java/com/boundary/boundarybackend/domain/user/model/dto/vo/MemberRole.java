fb2458e155257262d480b452b5fd4e57594ac153
한태영 <s46422574@gmail.com>
2024-08-25 03:28:11 +0900

    FEAT :: MemberRole enum 구현

---
+++ src/main/java/com/boundary/boundarybackend/domain/user/model/dto/vo/MemberRole.java
@@ -0,0 +1,16 @@
+package com.boundary.boundarybackend.domain.user.model.dto.vo;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum MemberRole
+{
+    Child("Child"),
+    Parent("Parent");
+
+
+    private final String role;
+}
+
