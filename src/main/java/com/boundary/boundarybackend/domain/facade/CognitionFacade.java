b480fcf7fa615297bb0f65bed3ecb7445b4865bb
noahmik <s46422574@gmail.com>
2024-09-03 20:13:16 +0900

    FIX :: CognitionFacade 재구현

--- src/main/java/com/boundary/boundarybackend/domain/facade/CognitionFacade.java
+++ src/main/java/com/boundary/boundarybackend/domain/facade/CognitionFacade.java
@@ -1,2 +1,19 @@
-package com.boundary.boundarybackend.domain.facade;public class CognitionFacade {
+package com.boundary.boundarybackend.domain.facade;
+
+import com.boundary.boundarybackend.domain.cognition.service.CognitionService;
+import com.boundary.boundarybackend.domain.cognition.model.dto.CognitionRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+public class CognitionFacade {
+    private final CognitionService cognitionService;
+
+    @Transactional
+    public void cognition(CognitionRequest req, Long id) {
+        // CognitionService를 사용해 User의 포인트를 업데이트한다.
+        cognitionService.updateUserPoints(req, id);
+    }
 }
