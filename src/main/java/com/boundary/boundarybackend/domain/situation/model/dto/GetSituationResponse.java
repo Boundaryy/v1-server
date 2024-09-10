ea20b0277a12745a0a22ba384d75c86340089b5a
YunChan-Oh <oyunchan9@gmail.com>
2024-09-10 22:44:09 +0900

    FEAT :: GetSituationResponse 구현

---
+++ src/main/java/com/boundary/boundarybackend/domain/situation/model/dto/GetSituationResponse.java
@@ -0,0 +1,23 @@
+package com.boundary.boundarybackend.domain.situation.model.dto;
+
+import com.boundary.boundarybackend.domain.situation.model.entity.Situation;
+
+import java.time.LocalDateTime;
+
+public record GetSituationResponse(
+        Long situationId,
+        String content,
+        String parentId,
+        String childId,
+        LocalDateTime createdAt
+) {
+    public static GetSituationResponse from(Situation situation) {
+        return new GetSituationResponse(
+                situation.getId(),
+                situation.getContent(),
+                situation.getParentId(),
+                situation.getChildId(),
+                situation.getCreatedAt()
+        );
+    }
+}
